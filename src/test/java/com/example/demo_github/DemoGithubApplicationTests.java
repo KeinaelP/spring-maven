39d14a74d7827f1080a64cc9e1157eb8edc355c5
KeinaelP <keinael.philemon.pro@gmail.com>
2025-06-04 10:54:10 +0200

    Update DemoGithubApplicationTests.java

--- src/test/java/com/example/demo_github/DemoGithubApplicationTests.java
+++ src/test/java/com/example/demo_github/DemoGithubApplicationTests.java
@@ -1,13 +1,14 @@
 package com.example.demo_github;
-
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class DemoGithubApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+    @Test
+    void contextLoads() {
+        
+    }
 }
